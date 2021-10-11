0552189406e3a2ac90ec6fa01126ede01c5a6986
moratomicro <moratomicro@yahoo.com.br>
2021-10-10 22:28:43 -0300

    Criando arquivo main.js, que conterá todos os códigos JavaScript da página html

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var nome = "Reine Alexandre Morato";
+var idade = 46;
+var idade2 = 10
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos.");
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
